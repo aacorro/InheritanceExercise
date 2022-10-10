4981281ab5ccd57418ba0e6c68bbab76bb44de8b
Abdiel Corro <acorrop@outlook.com>
2022-10-10 01:45:12 -0500

    deleted instructions comments

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,29 +6,7 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
-
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
-
-            
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             * 
-             * Creatively display the class member values 
-             */
-
+  
 
             Bird falcon = new Bird();
             falcon.beakShape = "raptorial";
@@ -36,14 +14,6 @@ namespace Inheritance
             falcon.numSpecies = 19;
             falcon.migration = true;
 
-
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             * 
-             * 
-             */
 
             Reptile turtle = new Reptile();
             turtle.numSpecies = 356;
